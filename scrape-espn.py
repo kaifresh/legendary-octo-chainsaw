5a37f247046f972e9eacdf9de9f5d509fbb43f56
Kai Tan <tan.kairen@gmail.com>
2017-08-07 11:40:05 +1000

    save before removing old scrape attmpets

--- scrape-espn.py
+++ scrape-espn.py
@@ -77,16 +77,86 @@ def GetTeamData(team_anchor):
         "team_id": team_anchor.find("abbr")["title"],
         "W/L": [],
         "Score": [],
-        "Opp": []
+        "Opp": [],
+        "Date & Box": [],
     }
     print(team_data)
 
-    team_url = base_mlb+team_anchor['href']
-
-    # full_schedule_endpoint = os.path.dirname( team_anchor['href'].replace("team", "team/schedule") )
-    # team_url = base_mlb+full_schedule_endpoint
+    # team_url = base_mlb+team_anchor['href']
+    full_schedule_endpoint = os.path.dirname( team_anchor['href'].replace("team", "team/schedule") )
+    team_url = base_mlb+full_schedule_endpoint
 
     print(team_url)
+
+    team_html = requests.get(team_url, headers=header)
+
+    if team_html.status_code == 200:
+        soup = BeautifulSoup(team_html.content, 'html.parser')
+
+        game_rows = soup.findAll("tr")
+
+        ### August 2017, column header positions
+        date_col = 0
+        opponent_col = 1
+        result_col = 2
+        winning_pitcher_col = 3
+        losing_pitcher_col = 3
+
+
+
+        for game in game_rows:
+            print("~ " * 100)
+            # print(game.prettify())
+
+
+            # Who / Where
+            # Score
+            # W/L
+
+            cells = game.findAll("td")
+
+
+            if len(cells) > 1:
+                opposing_team = cells[ opponent_col ].findAll(text=True)
+                result_and_score = cells[ result_col ].findAll(text=True)
+                date = cells[date_col].findAll(text=True)
+
+
+                if len(opposing_team) == 2 and len(result_and_score) == 2 and len(date) == 1:
+
+                    print(date)
+                    print(opposing_team)
+                    print(result_and_score)
+
+                    result = result_and_score[0]
+                    score = result_and_score[1]
+                    date = date[0]
+                    opposing_team = ' '.join(opposing_team)
+
+
+
+
+
+
+            # print("." * 200)
+            # win_lose = game.findAll("li", {"class": "game-status"})
+            # score = game.findAll("li", {"class": "score"})
+            # opposing_team = game.findAll("ul", {"class": "game-schedule"})
+            #
+            # if len(win_lose) > 0 and len(score) > 0 and len(opposing_team) > 0 :
+            #
+            #     # win_lose = win_lose[0].find(text=True)
+            #     # score = score[0].find(text=True)
+            #     # opposing_team = opposing_team[0].find(text=True)
+            #
+            #
+            #     print(win_lose)
+            #
+            #     print(score)
+            #     print(opposing_team)
+            #     print("." * 200)
+
+
 
     # team_html = requests.get(team_url, headers=header)
 
@@ -99,7 +169,7 @@ def GetTeamData(team_anchor):
     #
     #     for game in prev_games_info:
     #
-    #         win_lose = game.find("div", "game-result")
+    #         win_lose = game.find("div", "game-result_and_score")
     #         score = game.find("div", "score")
     #
     #         opposing_team = game.parent.find("div", {"class": "game-info"})
