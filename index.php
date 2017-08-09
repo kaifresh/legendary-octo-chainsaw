1fc411dbf42da7bd1284fac496cf4d0519b6067f
Kai Tan <tan.kairen@gmail.com>
2017-08-09 13:25:48 +1000

    php long running process wrangling

--- index.php
+++ index.php
@@ -11,40 +11,7 @@ Getting Todays MLB Info. Please wait.
 
 <?php
 
-function run_cmd_with_output($cmd){
-
-    $descriptorspec = array(
-       0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
-       1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
-       2 => array("pipe", "w")    // stderr is a pipe that the child will write to
-    );
-    flush();
-    $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
-    echo "<pre>";
-    if (is_resource($process)) {
-        while ($s = fgets($pipes[1])) {
-            print $s;
-            flush();
-        }
-    }
-    echo "</pre>";
-
-}
-
-
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
-echo "<b>TEST</b>";
-$cmd = 'ls ./';
-
-run_cmd_with_output($cmd);
-
-$cmd = 'source ./bin/activate && python scrape_espn.py';
-
-run_cmd_with_output($cmd);
-
+print `echo /usr/bin/php -q scrape.php | at now`;
 
 
 ?>
